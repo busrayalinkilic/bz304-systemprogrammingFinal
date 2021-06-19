20a05c7dfd3e73842c5b2315bc515e6ae60817ae
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-06-19 21:44:56 +0300

    break-continue statement

---
+++ break-continue-statement.py
@@ -0,0 +1,17 @@
+#break
+
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+print("The end")
+
+#continue
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
